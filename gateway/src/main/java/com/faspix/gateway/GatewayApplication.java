10f05bfbd07e6c113fa74c26c4d1bc61a1f52878
faspix <babkoum05@gmail.com>
2025-01-24 15:22:51 +0100

    feat: gateway implementation

--- gateway/src/main/java/com/faspix/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/faspix/gateway/GatewayApplication.java
@@ -1,7 +1,15 @@
 package com.faspix.gateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class GatewayApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
