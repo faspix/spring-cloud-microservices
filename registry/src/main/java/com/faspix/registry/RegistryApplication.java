058cd491633545a07f80701501b7d110714bcbcb
faspix <babkoum05@gmail.com>
2025-01-24 15:24:36 +0100

    feat: registry implementation

--- registry/src/main/java/com/faspix/registry/RegistryApplication.java
+++ registry/src/main/java/com/faspix/registry/RegistryApplication.java
@@ -1,7 +1,13 @@
 package com.faspix.registry;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
 public class RegistryApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(RegistryApplication.class, args);
     }
 }
