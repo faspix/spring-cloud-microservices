c9e67eeb84ecabfefe198dedc237dd64ff0d1c1c
faspix <babkoum05@gmail.com>
2025-02-04 18:10:04 +0100

    feat: add exception handlers

---
+++ account-service/src/main/java/com/faspix/account/exception/ExceptionHandlerImpl.java
@@ -0,0 +1,22 @@
+package com.faspix.account.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ExceptionHandlerImpl {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    private ExceptionResponse handleAccountNotFountException(final AccountNotFoundException e) {
+        return new ExceptionResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    private ExceptionResponse handleGeneralException(final Exception e) {
+        return new ExceptionResponse(e.getMessage());
+    }
+
+}
---
+++ deposit-service/src/main/java/com/faspix/deposit/exception/ExceptionResponse.java
@@ -0,0 +1,12 @@
+package com.faspix.deposit.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+class ExceptionResponse {
+
+    private final String message;
+
+}
